24361ceba8d91a189e881b8db952cda1feaafd74
juanshen <gyanzhuan051@gmil.com>
2026-03-17 00:18:09 +0800

    feat: add linux.do RSS support

--- src/config/platforms.js
+++ src/config/platforms.js
@@ -175,6 +175,7 @@ export const PLATFORMS = {
   arxiv: 'https://arxiv.org',
   fdroid: 'https://f-droid.org',
   jenkins: 'https://updates.jenkins.io',
+  'linux-do': 'https://linux.do',
 
   // AI Inference Providers
   'ip-openai': 'https://api.openai.com',
